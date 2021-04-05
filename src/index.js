295078b1d991a449c31683f07fb6e136b40b95a2
jackie <jackienomsa3@gmail.com>
2021-04-05 12:19:47 +0200

    Hash

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../src/index.css";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 const ReactRouterSetup = () => {
   return <App />;
 };
 ReactDOM.render(
   <React.StrictMode>
-    <ReactRouterSetup />
+    <HashRouter>
+      <ReactRouterSetup />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
